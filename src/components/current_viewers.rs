158c6daca50fc2c714bf5bfab0ad753de820d799
Marcus Hann <marcus@laptop.hhra.uk>
2023-07-19 21:22:41 +0100

    Cleanup a little

--- src/components/current_viewers.rs
+++ src/components/current_viewers.rs
@@ -6,44 +6,12 @@ use leptos_server_signal::create_server_signal;
 use serde::{Deserialize, Serialize};
 use leptos_heroicons::size_24::solid::{CheckCircle, XCircle};
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct TautulliResponse<T> {
-    response: T,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct TautulliDataResponse<T> {
-    data: T,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct TautulliActivityData {
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    stream_count: u32,
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    wan_bandwidth: u32,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct TautulliActivityStreamData {
-    #[serde(deserialize_with = "deserialize_number_from_string")]
-    bandwidth: u32,
-    location: String,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Stats {
-    total_bandwidth: u32,
-    stream_count: u32,
-}
-
 #[component]
 pub fn CurrentViewers(cx: Scope) -> impl IntoView {
     let count = create_server_signal::<Count>(cx, "counter");
 
     view! {
         cx,
-        <>
         <div class="grid h-screen place-items-center">
              <StatusBox>
                  { move || if count().total_bandwidth > 7 && count().total_bandwidth <= 12 {
@@ -51,13 +19,11 @@ pub fn CurrentViewers(cx: Scope) -> impl IntoView {
                  } else if count().total_bandwidth > 12 {
                      view!{cx, <CheckCircle class="w-16 h-16 text-emerald-600" /> }
                  } else {
-                     view! {cx,
-                         <XCircle class="w-16 h-16 text-red-600" /> }
+                     view!{cx, <XCircle class="w-16 h-16 text-red-600" /> }
                  }}
-                 <p class="w-full">"Stream Count: " {move || count().stream_count.to_string()}</p>
-                 <p class="w-full">"Available Bandwidth: " {move || count().total_bandwidth.to_string()} "Mbps"</p>
+                 <p>"Stream Count: " {move || count().stream_count.to_string()}</p>
+                 <p>"Available Bandwidth: " {move || count().total_bandwidth.to_string()} "Mbps"</p>
             </StatusBox>
         </div>
-        </>
     }
 }
